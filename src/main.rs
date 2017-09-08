4b4ad8adfb6d2be4212e3fd586f485584fdbc95a
Brandon Thomas <echelon@gmail.com>
2017-09-08 02:37:32 -0400

    Getting started

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// Copyright (c) 2017 Brandon Thomas <bt@brand.io>
+// 2017 Atlanta Beltline Lantern Parade Laser Demo
+
+extern crate argparse;
+extern crate beam;
+extern crate lase;
+
+fn main() {
+  println!("Just getting started");
+}
